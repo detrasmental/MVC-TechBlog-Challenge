615da9c866c3ea0d9eedc86089b009a56168c18a
detrasmental <detra.gordon76@gmail.com>
2021-12-20 18:28:33 -0500

    finished!

---
+++ server.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+
+const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const oneDay = 1000 * 60 * 60 * 24;
+const sess = {
+  secret: 'thisismysecretkey',
+  cookie: {
+    maxAge: oneDay
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
