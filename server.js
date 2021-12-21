1e0585243a31ed4c87bbd3a9fb27db17b1b0b5d5
detrasmental <detra.gordon76@gmail.com>
2021-12-21 10:36:16 -0500

    ...

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 const helpers = require('./utils/helpers');
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
-
 const session = require('express-session');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -38,7 +37,6 @@ app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
