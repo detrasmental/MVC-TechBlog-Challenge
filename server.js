6eb7ff95e12ce09c90b8a64a3e84f98f0198254e
detrasmental <detra.gordon76@gmail.com>
2021-12-21 08:14:06 -0500

    still heroku

--- server.js
+++ server.js
@@ -39,7 +39,6 @@ app.set('view engine', 'handlebars');
 app.use(routes);
 
 // turn on connection to db and server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
