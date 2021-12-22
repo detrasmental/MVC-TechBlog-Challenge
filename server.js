46241ee0b9e74a7217ff4cd8d28384b1f076b1e9
detrasmental <detra.gordon76@gmail.com>
2021-12-21 19:41:47 -0500

    please work!

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(routes);
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
