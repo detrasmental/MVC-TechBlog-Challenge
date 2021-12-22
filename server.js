5a4562de586374c9822cc3cd03de33b7c42e8f89
detrasmental <detra.gordon76@gmail.com>
2021-12-21 19:52:36 -0500

    ...

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const path = require('path');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 const helpers = require('./utils/helpers');
 
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
-
-const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 
 const sess = {
   secret: 'thisismysecretkey',
@@ -23,19 +19,21 @@ const sess = {
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
-  }),
+  })
 };
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(routes);
+app.use(require('./controllers/'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
