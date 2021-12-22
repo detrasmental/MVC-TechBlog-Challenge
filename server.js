61612d6bc8b3b7ade8ec3655a6aab23543265548
detrasmental <detra.gordon76@gmail.com>
2021-12-21 20:37:31 -0500

    please work!

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const exphbs = require("express-handlebars");
 const helpers = require('./utils/helpers');
 const routes = require('./controllers');
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
