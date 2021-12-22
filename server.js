7a9e8466f0083727c7b657de5dddf18db8b68f50
detrasmental <detra.gordon76@gmail.com>
2021-12-21 19:50:46 -0500

    ...

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
